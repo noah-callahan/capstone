70b5a860ecf497ac9dec358d023d7bcda28345cc
Noah Callahan <noahc1991@gmail.com>
2018-03-22 17:32:49 -0400

    point system half working

--- main.js
+++ main.js
@@ -2,20 +2,43 @@
 
 var words = ["hotel","philadelphia","recliner","summer","programmer","subway","airplane","mouse","spaghetti","skateboard","bicycle"]
 
-function randomWord(){
-    return words[Math.floor(Math.random() * words.length)];
-};
+var hints = ["A place you stay on vacation", "A city","Something you sit on", "A season", "A profession", "A form of public transportation","A form of transportation", "An animal or computer peripheral","A food", "Bart Simpson likes to ride this","A form of transportation driven by exertion"]
 
+var points = [10, 25, 50, 75, 100];
+
+var worth = null;
 var word = null;
 var badTry = 0;
 var goodTry = 0;
 var input = null;
 var wordArr = null;
+var index = null;
+var score = null;
+
+function randomWord(){
+    return words[Math.floor(Math.random() * words.length)];
+};
+
+worth = points[Math.floor(Math.random() * points.length)];
+
+function randomPoints(){
+    return worth;
+};
 
 document.getElementById("random-word").addEventListener("click", function(){
     document.getElementById("word").innerHTML = word = randomWord();
     document.getElementById("length").innerHTML = word.length;
+    index = words.indexOf(word);
+    document.getElementById("hint").innerHTML = hints[index];
     wordArr = word.split('');
+    randomPoints();
+    console.log(worth);
+    document.getElementById("turn-points").innerHTML = worth;
+});
+
+
+document.getElementById("reset").addEventListener("click", function(){
+    location.reload();
 });
 
 
@@ -24,17 +47,30 @@ function myGuess() {
     input = document.getElementById("userInput").value;
         if(wordArr.includes(input)){
             for(i = 0; i < wordArr.length; i++)
-            if (wordArr[i] == input){
-                div = String(i);
-                a = wordArr[i];
-                document.getElementById(div).innerHTML = input;
-            } 
-        } else {
-            alert("Sorry, try again!");
-        }
+                if (wordArr[i] == input){
+                    div = String(i);
+                    a = wordArr[i];
+                    document.getElementById(div).innerHTML = input;
+                    goodTry++;
+                    score += worth;
+                    document.getElementById("total-score").innerHTML = score;
+                    document.getElementById("turn-points").innerHTML = worth;
+                }
+                } else {
+                    alert("Sorry, try again!");
+                    badTry++;
+                    document.getElementById("bad-try").innerHTML = badTry;
+                    document.getElementById("turn-points").innerHTML = worth;
+                } if(badTry == 4){
+                    alert("Sorry, you lost!");
+                    location.reload();
+                }
 }
 
+//worth = points[Math.floor(Math.random() * points.length)]
 
+
+//random points return worth
 
     // if(wordArr.includes(input)){
     //     alert("Yup, that's in the word.");
